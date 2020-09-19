5714a120854fe6c21d3b8816ee97bb1fd0dd4f66
pulssolidarite <contact@puls-impact.fr>
2020-09-19 13:17:23 +0200

    debug authentification

--- src/renderer/views/Home.vue
+++ src/renderer/views/Home.vue
@@ -242,6 +242,7 @@ export default {
         this.loading = false;
       })
       .catch((err) => {
+	console.log(err.response);
         this.errors = {
           visible: true,
           title: "Erreur de chargement",
