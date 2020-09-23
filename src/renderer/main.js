a6aa2de8bac497bfae5159bae05663f29d538a7d
Moncef HILALI <hilali.moncef@gmail.com>
2020-09-23 22:32:35 +0200

    changing auth mode

--- src/renderer/main.js
+++ src/renderer/main.js
@@ -5,7 +5,7 @@ import store from "./store";
 import axios from "axios";
 import "bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
-import 'vueperslides/dist/vueperslides.css'
+import "vueperslides/dist/vueperslides.css";
 import "./assets/css/theme.css";
 import { library } from "@fortawesome/fontawesome-svg-core";
 import {
@@ -17,11 +17,12 @@ import {
   faLink,
   faEuroSign,
   faPlay,
-  faVideo
+  faVideo,
 } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
 import VueProgressBar from "vue-progressbar";
 import VueYoutube from "vue-youtube";
+import interceptors from "./axios/index.js";
 
 Vue.use(VueYoutube);
 
@@ -29,7 +30,7 @@ Vue.use(VueYoutube);
 Vue.use(VueProgressBar, {
   color: "rgb(91, 184, 93)",
   failedColor: "red",
-  height: "3px"
+  height: "3px",
 });
 
 // Font Awesome Stuff
@@ -51,14 +52,19 @@ Vue.config.productionTip = false;
 // Axios stuff
 Vue.prototype.$http = axios;
 //Vue.prototype.$http.defaults.baseURL = process.env.SERVER_URL;
-Vue.prototype.$http.defaults.baseURL = "https://puls-preprod.herokuapp.com/";
-const token = localStorage.getItem("token");
+//Vue.prototype.$http.defaults.baseURL = "https://puls-preprod.herokuapp.com/";
+Vue.prototype.$http.defaults.baseURL = "http://127.0.0.1:8000/";
+const token = localStorage.getItem("accessToken");
 if (token) {
-  Vue.prototype.$http.defaults.headers.common["Authorization"] = token;
+  Vue.prototype.$http.defaults.headers.common[
+    "Authorization"
+  ] = `Bearer ${token}`;
 }
+// We create the interceptors for AXIOS 401 responses
+interceptors.createAxiosResponseInterceptor();
 
 new Vue({
   router,
   store,
-  render: h => h(App)
+  render: (h) => h(App),
 }).$mount("#app");
